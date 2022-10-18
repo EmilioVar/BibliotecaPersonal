d6a9ca9ebd509b72720ead690de008e788bb8eb4
Emilio <emiliovm@outlook.es>
2022-10-18 22:35:38 +0200

    navbar fixed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,23 +12,7 @@
 
 </head>
 <body>
-    <nav style="width:100vw;height:80px;background-color:#f0f0f0;">
-        <ul>
-            <p>index</p>
-        </ul>
-        @if (Route::has('login'))
-      <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-           @auth
-                  <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-           @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                    @endif
-                @endauth
-            </div>
-@endif
-    </nav>
+    <x-nav-bar />
     <div class="container-fluid">
         <div class="row">
             <div class="col-3">
